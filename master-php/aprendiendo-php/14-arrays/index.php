6a6b727ccee6edc7376fc9d4a238bfeaa2e0a6d9
Juan Bedoya <extibax@gmail.com>
2019-01-16 18:49:44 -0500

    Arrays

--- master-php/aprendiendo-php/14-arrays/index.php
+++ master-php/aprendiendo-php/14-arrays/index.php
@@ -10,6 +10,13 @@
 $pelicula = "Batman";
 $peliculas = array('Batman', 'Spiderman', 'El señor de los anillos');
 $cantantes = ['2pac', 'Drake', 'Jennifer Lopez'];
+$personas = array(
+    'nombre' => 'Victor',
+    'apellidos' => 'Robles',
+    'Web' => 'extibax.com'
+);
+
+var_dump($personas);
 
 var_dump($peliculas);
 
@@ -20,3 +27,19 @@ var_dump($peliculas);
 echo $peliculas[0];
 echo "<br>";
 echo $cantantes[2];
+
+/* Recorrer con FOR */
+echo "<h1>Listado de peliculas</h1>";
+echo "<ul>";
+for ($i=0; $i < count($peliculas); $i++) { 
+    echo "<li>".$peliculas[$i]."</li>";
+}
+echo "</ul>";
+
+/* Recorrer con FOREACH */
+echo "<ul>";
+foreach ($cantantes as $cantante) {
+    echo "<li>".$cantante."</li>";
+}
+echo "</ul>";
+

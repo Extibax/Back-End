af4c9fbc6c2386788182e2e76d167d6999f55d81
Juan Bedoya <extibax@gmail.com>
2019-01-16 12:14:49 -0500

    Includes

---
+++ master-php/aprendiendo-php/13-includes/includes/cabecera.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang=es>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Includes en PHP 7</title>
+</head>
+<body>
+    <!-- Cabecera -->
+    <div class="cabecera">
+        <h1>Includes en PHP7</h1>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="sobremi.php">Sobre mi</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+            <li><a href="https://google.com">Google</a></li>
+        </ul>
+        <hr>
+    </div>
---
+++ master-php/aprendiendo-php/13-includes/index.php
@@ -0,0 +1,9 @@
+<?php require_once 'includes/cabecera.php'; ?>
+
+    <!-- Contenido -->
+    <div>
+        <h2>Esta es la pagina de inicio</h2>
+        <p>Texto de prueba de la pagina de inicio</p>
+    </div>
+
+<?php require_once 'includes/footer.php'; ?>

addfad641df451019c17f850b56432f3ef3a4011
Juan Bedoya <extibax@gmail.com>
2019-01-19 15:28:41 -0500

    Cookies

---
+++ master-php/aprendiendo-php/17-cookies/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+    Cokkie: Es un fichero que se almacena en el ordenador del usuario que visita
+    la web, con el fin de recordar datos o rastrear cierta informacion o comportamiento
+    del mismo en la web.
+*/
+
+/* Crear cookie */
+/* setcookie("nombre", "valorQueSoloPuedeSerTexto", caducidad, ruta, dominio); */
+
+/* Cookie basica */
+setcookie("miCookie", "Valor de mi Cookie");
+

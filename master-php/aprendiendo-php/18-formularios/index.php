51d22ba4df1d7f5d700a615aed176bca4728faa5
Juan Bedoya <extibax@gmail.com>
2019-01-19 17:37:12 -0500

    Formularios

--- master-php/aprendiendo-php/18-formularios/index.php
+++ master-php/aprendiendo-php/18-formularios/index.php
@@ -13,20 +13,46 @@
     <form action="" method="POST" enctype="multipart/form-data">
         <label for="nombre">Nombre:</label>
         <p><input type="text" name="nombre" id="" autofocus></p>
+
         <label for="apellido">Apellido:</label>
         <p><input type="text" name="apellido" pattern="[A-Z ]+"></p>
-        <label for="">Boton:</label>
-        <p><input type="submit" value="Enviar"></p>
+
         <label for="">Sexo:</label>
         <p>
             Hombre <input type="checkbox" name="sexo" value="Hombre">
             Mujer <input type="checkbox" name="sexo" value="Mujer">
         </p>
 
-        <label for="color">Color: </label>
+        <label for="fecha">Color:</label>
         <p><input type="color" name="color"></p>
 
-        label
+        <label for="fecha">Fecha:</label>
+        <p><input type="date" name="apellido"></p>
+
+        <label for="correo">Email:</label>
+        <p><input type="email" name="correo" id=""></p>
+
+        <label for="archivo">Archivo:</label>
+        <p><input type="file" name="archivo" id=""></p>
+
+        <label for="numero">Numero:</label>
+        <p><input type="number" name="numero" id=""></p>
+
+        <label for="contraseña">Contraseña:</label>
+        <p><input type="password" name="contraseña" id=""></p>
+
+        <label for="continente">Continente:</label>
+        <p>
+            Sur America <input type="radio" name="continente" id="" value="America del Sur">
+            Norte America <input type="radio" name="continente" id="" value="America del Norte">
+            Asia <input type="radio" name="continente" id="" value="America del Asia">
+        </p>
+
+        <label for="url">URL:</label>
+        <p><input type="url" name="url" id=""></p>
+
+        <label for="">Boton:</label>
+        <p><input type="submit" value="Enviar"></p>
     </form>
 </body>
 

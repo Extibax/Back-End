83cf9deb52ca2e78599f5a73b55aea470788153a
Juan Bedoya <extibax@gmail.com>
2019-01-20 21:33:11 -0500

    Modificar

--- master-php/aprendiendo-sql/02-modificar_tablas/modificar.sql
+++ master-php/aprendiendo-sql/02-modificar_tablas/modificar.sql
@@ -2,4 +2,17 @@
 ALTER TABLE usuarios RENAME TO usuarios_renom;
 
 # Cambiar el nombre de una columna #
-ALTER TABLE usuarios_renom CHANGE apellidos apellido VARCHAR(100) null
+ALTER TABLE usuarios_renom CHANGE apellidos apellido VARCHAR(100) null;
+
+# Modificar columna si modificar su nombre #
+ALTER TABLE usuarios_renom MODIFY apellido CHAR(50) NOT NULL;
+
+# Añadir una nueva columna #
+ALTER TABLE usuarios_renom ADD website VARCHAR(100) NULL;
+
+# Añadir restriccion a columna #
+ALTER TABLE usuarios_renom ADD CONSTRAINT uq_email UNIQUE(email);
+
+# Eliminar una columna #
+ALTER TABLE usuarios_renom DROP website;
+

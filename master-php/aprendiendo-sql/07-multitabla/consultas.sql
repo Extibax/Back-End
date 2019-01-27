4546347707b990a287369cbe55c4d83865876299
Juan Bedoya <extibax@gmail.com>
2019-01-26 22:20:50 -0500

    Multitablas

---
+++ master-php/aprendiendo-sql/07-multitabla/consultas.sql
@@ -0,0 +1,17 @@
+# 
+Consulta multitabla: Son consultas que sirven para consultar varias tablas en una
+sola sentencia
+#
+
+# Mostrar las entradas con el nombre del autor y el nombre de la categoria #
+SELECT e.id, e.titulo, u.nombre AS 'Autor', c.nombre AS 'Categoria' 
+FROM entradas e, usuarios u, categorias c
+WHERE e.usuario_id = u.id AND e.categoria_id = c.id;
+
+# Mostrar el nombre de las categorias y al lado cuantas entradas tiene #
+SELECT c.nombre, COUNT(e.id) FROM categorias c, entradas e 
+WHERE e.categoria_id = c.id GROUP BY e.categoria_id;
+
+# Mostrar el email de los usuarios y al lado cuantas entradas tiene #
+SELECT u.email, COUNT(titulo) FROM usuarios u, entradas e
+WHERE e.usuario_id = u.id GROUP BY e.usuario_id;

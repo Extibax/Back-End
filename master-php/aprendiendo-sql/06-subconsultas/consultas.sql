f5c13fa4f58ce5e4371f65a453a3877bd991f534
Juan Bedoya <extibax@gmail.com>
2019-01-26 21:57:50 -0500

    Subconsultas

--- master-php/aprendiendo-sql/06-subconsultas/consultas.sql
+++ master-php/aprendiendo-sql/06-subconsultas/consultas.sql
@@ -13,3 +13,23 @@ SELECT * FROM usuarios WHERE id NOT IN (SELECT usuario_id FROM entradas);
 # Sacar los usuarios que tengan alguna entrada que en su titulo hable de GTA V #
 SELECT nombre, apellidos FROM usuarios WHERE id 
     IN (SELECT usuario_id FROM entradas WHERE titulo LIKE "%GTA%");
+
+# Sacar todas las entradas de la categoria accion utilizando su nombre #
+SELECT categoria_id, titulo FROM entradas WHERE categoria_id
+    IN (SELECT id FROM categorias WHERE nombre = 'Accion');
+
+# Mostrar las categorias con mas de 3 entradas #
+SELECT nombre FROM categorias WHERE 
+    id IN (SELECT categoria_id FROM entradas GROUP BY categoria_id HAVING COUNT(categoria_id) >=3);
+
+# Mostrar los usuarios que crearon una entrada un martes #
+SELECT * FROM usuarios WHERE id 
+    IN (SELECT usuario_id FROM entradas WHERE DAYOFWEEK(fecha) = 3);
+
+# Mostrar el nombre del usuario que tenga mas entradas #
+SELECT nombre FROM usuarios WHERE id 
+    = (SELECT usuario_id FROM entradas GROUP BY usuario_id ORDER BY COUNT(id) DESC LIMIT 1);
+
+# Mostrar las categorias sin entradas #
+SELECT * FROM categorias WHERE id 
+    NOT IN (SELECT categoria_id FROM entradas);

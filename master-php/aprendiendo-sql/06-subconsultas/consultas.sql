082efc87df8dd7ccd12ab4ba578e4b42d5cdf8b1
Juan Bedoya <extibax@gmail.com>
2019-01-26 20:01:20 -0500

    Subconsultas

---
+++ master-php/aprendiendo-sql/06-subconsultas/consultas.sql
@@ -0,0 +1,12 @@
+# 
+*Subconsultas: Son consultas que se ejecutan dentro de otras, consiste en utilizar
+los resultados de la subconsulta para operar en la consulta principal.
+*Jugando con las clabes ajenas / foraneas.
+#
+
+# Mostrar registros que su id este dentro de la tabla entradas #
+SELECT * FROM usuarios WHERE id IN (SELECT usuario_id FROM entradas);
+
+# Mostrar registros que su id no esten dentro de la tabla entradas #
+SELECT * FROM usuarios WHERE id NOT IN (SELECT usuario_id FROM entradas);
+

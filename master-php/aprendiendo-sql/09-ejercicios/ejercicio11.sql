1e5d5a05b535e4ad3d830cf154aaf74d4fa97cd5
Juan Bedoya <extibax@gmail.com>
2019-01-30 12:44:13 -0500

    Ejercicios

--- master-php/aprendiendo-sql/09-ejercicios/ejercicio11.sql
+++ master-php/aprendiendo-sql/09-ejercicios/ejercicio11.sql
@@ -2,4 +2,4 @@
 11. Visualizar todos los cargos y el numero de vendedores que hay en cada cargo
 */
 
-SELECT cargo, COUNT(id) AS cantidad FROM vendedores GROUP BY cargo;
+SELECT cargo, COUNT(id) AS cantidad FROM vendedores GROUP BY cargo ORDER BY(cantidad) DESC;

00fb28e7f906950bc42e141a766389c6c1be1c0d
Juan Bedoya <extibax@gmail.com>
2019-01-31 20:32:20 -0500

    Ejercicio 13

--- master-php/aprendiendo-sql/09-ejercicios/ejercicio13.sql
+++ master-php/aprendiendo-sql/09-ejercicios/ejercicio13.sql
@@ -1,5 +1,5 @@
 /* 
 13. Sacar la media de sueldos entre todos los vendedores por grupo
 */
-SELECT CEIL (AVG(v.sueldo)) AS 'Media Salarial', g.nombre, g.ciudad 
+SELECT CEIL(AVG(v.sueldo)) AS 'Media Salarial', g.nombre, g.ciudad 
 FROM vendedores v INNER JOIN grupos g ON g.id = v.grupo_id GROUP BY grupo_id;

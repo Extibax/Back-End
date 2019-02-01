40d1d99f371a760a717cfd21563248bb85d15233
Juan Bedoya <extibax@gmail.com>
2019-02-01 18:30:45 -0500

    Ejercicio 17

--- master-php/aprendiendo-sql/09-ejercicios/ejercicio17.sql
+++ master-php/aprendiendo-sql/09-ejercicios/ejercicio17.sql
@@ -2,7 +2,16 @@
 17. Obtener listado con los encargos realizados por el cliente 'Fruteria Antonia INC'
 */
 
-SELECT c.Name AS 'Cliente', o.Quantity AS 'Cantidad de pedidos' 
-FROM orders o 
-INNER JOIN customers c ON c.id = o.Customer_id
-WHERE c.Name = 'Fruteria Antonia INC';
+SELECT cus.Name AS 'Cliente', ord.Quantity AS 'Cantidad de pedidos' 
+FROM orders ord 
+INNER JOIN customers cus ON cus.id = ord.Customer_id
+WHERE cus.Name = 'Fruteria Antonia INC'; 
+
+# OR #
+
+SELECT ord.id AS 'Nº Encargo', ord.Quantity, cus.Name, ca.Model, ord.Date 
+FROM orders ord 
+INNER JOIN customers cus ON cus.id = ord.Customer_id 
+INNER JOIN cars ca ON ca.id = ord.Car_id
+WHERE ord.Customer_id IN 
+(SELECT id FROM customers WHERE Name = 'Fruteria Antonia INC');

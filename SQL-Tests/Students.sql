20bb4490427f6e83ed652e884bcc8642c8824e93
Extibax <extibax@gmail.com>
2019-03-03 00:08:17 -0500

    SQL Sentences Finished

--- SQL-Tests/Students.sql
+++ SQL-Tests/Students.sql
@@ -2,108 +2,114 @@ CREATE DATABASE Estudiantes CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 USE Estudiantes;
 
-CREATE TABLE Estudiantes
+CREATE TABLE Estudiante
 (
-    ID                  INT(11) AUTO_INCREMENT NOT NULL,
-    Nombre              VARCHAR(255) NOT NULL,
-    Primer_apellido     VARCHAR(255) NOT NULL,
-    Segundo_apellido    VARCHAR(255),
-    CONSTRAINT pk_estudiantes PRIMARY KEY(ID)
+    cod_Est             INT(11) AUTO_INCREMENT NOT NULL,
+    nombre_Est          VARCHAR(255) NOT NULL,
+    primer_Ape_Est      VARCHAR(255) NOT NULL,
+    segundo_Ape_Est     VARCHAR(255),
+    CONSTRAINT pk_estudiante PRIMARY KEY(cod_Est)
 );
 
-CREATE TABLE Colegios
+CREATE TABLE Colegio
 (
-    ID              INT(11) AUTO_INCREMENT NOT NULL,
-    Nombre          VARCHAR(255) NOT NULL,
-    Direccion       VARCHAR(255),
-    Telefono        VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_colegios PRIMARY KEY(ID)
+    nit_Col                 INT(11) AUTO_INCREMENT NOT NULL,
+    nombre_Col              VARCHAR(255) NOT NULL,
+    direccion_Col           VARCHAR(255),
+    telefono_col            VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_colegio PRIMARY KEY(nit_Col)
 );
 
-CREATE TABLE Grados
+CREATE TABLE Grado
 (
-    ID          INT(11) AUTO_INCREMENT NOT NULL,
-    Cupo        INT(10) NOT NULL,
-    Nombre      VARCHAR(100),
-    CONSTRAINT pk_grados PRIMARY KEY(ID)
+    cod_Gra             INT(11) AUTO_INCREMENT NOT NULL,
+    cupo_Gra            INT(10) NOT NULL,
+    nombre_Gra          VARCHAR(100),
+    CONSTRAINT pk_grado PRIMARY KEY(cod_Gra)
 );
 
-CREATE TABLE Matriculas
+CREATE TABLE Matricula
 (
-    ID              INT(11) AUTO_INCREMENT NOT NULL,
-    Estudiante_id   INT(11) NOT NULL,
-    Colegio_id      INT(11) NOT NULL,
-    Grado_id        INT(11) NOT NULL,
-    CONSTRAINT pk_matriculas PRIMARY KEY(ID),
-    CONSTRAINT fk_estudiantes FOREIGN KEY(Estudiante_id) REFERENCES Estudiantes(ID),
-    CONSTRAINT fk_colegios FOREIGN KEY(Colegio_id) REFERENCES Colegios(ID),
-    CONSTRAINT fk_grados FOREIGN KEY(Grado_id) REFERENCES Grados(ID)
+    cod_Mat             INT(11) AUTO_INCREMENT NOT NULL,
+    codigo_Est          INT(11) NOT NULL,
+    codigo_Col          INT(11) NOT NULL,
+    codigo_Gra          INT(11) NOT NULL,
+    CONSTRAINT pk_matricula PRIMARY KEY(cod_Mat),
+    CONSTRAINT fk_estudiante FOREIGN KEY(codigo_Est) REFERENCES Estudiante(cod_Est),
+    CONSTRAINT fk_colegio FOREIGN KEY(codigo_Col) REFERENCES Colegio(nit_Col),
+    CONSTRAINT fk_grado FOREIGN KEY(codigo_Gra) REFERENCES Grado(cod_Gra)
 );
 
-CREATE TABLE Grupos
+CREATE TABLE Grupo
 (
-    ID          INT(11) AUTO_INCREMENT NOT NULL,
-    Grado_id    INT(11) NOT NULL,
-    Nombre      VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_grupos PRIMARY KEY(ID),
-    CONSTRAINT fk_grados FOREIGN KEY(Grado_id) REFERENCES Grados(ID)
+    cod_Gru             INT(11) AUTO_INCREMENT NOT NULL,
+    codigo_Gra          INT(11) NOT NULL,
+    nombre_Grup         VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_grupo PRIMARY KEY(cod_Gru),
+    CONSTRAINT fk_grado FOREIGN KEY(codigo_Gra) REFERENCES Grado(cod_Gra)
 );
 
-CREATE TABLE Profesores
+CREATE TABLE Profesor
 (
-    ID              INT(11) AUTO_INCREMENT NOT NULL,
-    Materia_id      INT(11) NOT NULL,
-    Director        VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_profesores PRIMARY KEY(ID),
-    CONSTRAINT fk_materias FOREIGN KEY(Materia_id) REFERENCES Materias(ID)
+    cod_Pro                 INT(11) AUTO_INCREMENT NOT NULL,
+    codigo_Mat              INT(11) NOT NULL,
+    cod_Dir                 VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_profesor PRIMARY KEY(cod_Pro),
+    CONSTRAINT fk_materia FOREIGN KEY(codigo_Mat) REFERENCES Materia(cod_Mat)
 );
 
-CREATE TABLE Materias
+CREATE TABLE Materia
 (
-    ID                  INT(11) AUTO_INCREMENT NOT NULL,
-    Profesor_id         INT(11) NOT NULL,
-    Nombre              VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_materias PRIMARY KEY(ID),
-    CONSTRAINT fk_profesores FOREIGN KEY(Profesor_id) REFERENCES Profesores(ID)
+    cod_Mat                     INT(11) AUTO_INCREMENT NOT NULL,
+    codigo_Pro                  INT(11) NOT NULL,
+    nombre_Mat                  VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_materia PRIMARY KEY(cod_Mat),
+    CONSTRAINT fk_profesor FOREIGN KEY(codigo_Pro) REFERENCES Profesor(cod_Pro)
 );
 
-CREATE TABLE Cursos
+CREATE TABLE Curso
 (
-    ID              INT(11) AUTO_INCREMENT NOT NULL,
-    Grupo_id        INT(11) NOT NULL,
-    Materia_id      INT(11) NOT NULL,
-    CONSTRAINT pk_cursos PRIMARY KEY(ID),
-    CONSTRAINT fk_grupos FOREIGN KEY(Grupo_id) REFERENCES Grupos(ID),
-    CONSTRAINT fk_materias FOREIGN KEY(Materia_id) REFERENCES Materias(ID)
+    codigo_Cur                  INT(11) AUTO_INCREMENT NOT NULL,
+    codigo_Gru                  INT(11) NOT NULL,
+    codigo_Mat                  INT(11) NOT NULL,
+    CONSTRAINT pk_curso PRIMARY KEY(codigo_Cur),
+    CONSTRAINT fk_grupo FOREIGN KEY(codigo_Gru) REFERENCES Grupo(cod_Gru),
+    CONSTRAINT fk_materia FOREIGN KEY(codigo_Mat) REFERENCES Materia(cod_Mat)
 );
 
-CREATE TABLE Periodos
+CREATE TABLE Periodo
 (
-    ID          INT(11) AUTO_INCREMENT NOT NULL,
-    Fecha       DATE NOT NULL,
-    Numero      INT(11) NOT NULL,
-    CONSTRAINT pk_periodos PRIMARY KEY(ID)
+    cod_Pr              INT(11) AUTO_INCREMENT NOT NULL,
+    fecha_Per           DATE NOT NULL,
+    numero_Pr           INT(11) NOT NULL,
+    CONSTRAINT pk_periodo PRIMARY KEY(cod_Pr)
 );
 
-CREATE TABLE Evaluaciones
+CREATE TABLE Evaluacion
 (
-    ID              INT(11) AUTO_INCREMENT NOT NULL,
-    Materia_id      INT(11) NOT NULL,
-    Periodo_id      INT(11) NOT NULL,
-    Nombre          VARCHAR(255) NOT NULL,
-    Nota            FLOAT(11) NOT NULL,
-    Fecha           DATE NOT NULL,
-    Porcentaje      FLOAT(4),
-    CONSTRAINT pk_evaluaciones PRIMARY KEY(ID),
-    CONSTRAINT fk_materias FOREIGN KEY(Materia_id) REFERENCES Materias(ID),
-    CONSTRAINT fk_periodos FOREIGN KEY(Periodo_id) REFERENCES Periodos(ID)
+    cod_Ev              INT(11) AUTO_INCREMENT NOT NULL,
+    codigo_Mat          INT(11) NOT NULL,
+    codigo_Pr           INT(11) NOT NULL,
+    nombre_Ev              VARCHAR(255) NOT NULL,
+    nota_Ev                FLOAT(11) NOT NULL,
+    fecha_Ev               DATE NOT NULL,
+    procentaje_Ev          FLOAT(4),
+    CONSTRAINT pk_evaluacion PRIMARY KEY(cod_Ev),
+    CONSTRAINT fk_materia FOREIGN KEY(codigo_Mat) REFERENCES Materia(cod_Mat),
+    CONSTRAINT fk_periodo FOREIGN KEY(codigo_Pr) REFERENCES Periodo(cod_Pr)
 );
 
-CREATE TABLE Competencias
+CREATE TABLE Competencia
 (
-    ID                  INT(11) AUTO_INCREMENT NOT NULL,
-    Evaluacion_id       INT(11) NOT NULL,
-    Descripcion         VARCHAR(255) NOT NULL,
-    CONSTRAINT pk_competencias PRIMARY KEY(ID),
-    CONSTRAINT fk_evaluaciones FOREIGN KEY(Evaluacion_id) REFERENCES Evaluaciones(ID)
+    cod_Com                     INT(11) AUTO_INCREMENT NOT NULL,
+    codigo_Ev                   INT(11) NOT NULL,
+    descripcion_Com             VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_competencia PRIMARY KEY(cod_Com),
+    CONSTRAINT fk_evaluacione FOREIGN KEY(codigo_Ev) REFERENCES Evaluacion(cod_Ev)
 );
+
+SELECT E.cod_Est, E.primer_Ape_Est, E.segundo_Ape_Est, E.nombre_Est
+FROM Estudiante E, Matricula M, Grado G
+WHERE M.codigo_Est=E.cod_Est 
+AND M.codigo_Gra=G.cod_Gra 
+AND G.nombre_Gra='Septimo';

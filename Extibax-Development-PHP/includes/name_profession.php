7835a0cb26fb9054b7638aff86c3b8a56cd59b66
Extibax <extibax@gmail.com>
2019-03-15 15:58:11 -0500

    Deleted folder

--- Extibax-Development-PHP/includes/name_profession.php
+++
@@ -1,7 +0,0 @@
-<!-- Block Name & Profession -->
-<section class="name-profession">
-    <div class="container-text">
-        <h1 class="name">Extibax</h1>
-        <h2 class="profession">Back-End Developer</h2>
-    </div>
-</section>

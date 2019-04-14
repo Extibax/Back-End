977b45b42d902c07baec0fb6f4ac7f56f38d8fea
Extibax <extibax@gmail.com>
2019-04-13 19:15:36 -0500

    Call

---
+++ master-php/aprendiendo-php-poo/11-call/index.php
@@ -0,0 +1,29 @@
+<?php
+
+class Persona
+{
+    private $nombre;
+    private $edad;
+    private $ciudad;
+
+    function __construct($nombre, $edad, $ciudad)
+    {
+        $this->nombre = $nombre;
+        $this->edad = $edad;
+        $this->ciudad = $ciudad;
+    }
+
+    public function __call($name, $arguments)
+    {
+        $prefix_metodo = substr($name, 0, 3);
+        if ($prefix_metodo == 'get') {
+            $nombre_metodo = substr(strtolower($name), 3);
+            return $this->$nombre_metodo;
+        } else {
+            return "El metodo no existe";
+        }
+    }
+}
+
+$persona = new Persona("Juan Bedoya", 18, "Panama");
+echo $persona->getNombre();

9dba5065035cec1e9e90ebe03a91b404e69e7f06
Juan <extibax@gmail.com>
2019-08-23 12:48:07 -0500

    files added to the practice of  generate pdf

---
+++ master-php/aprendiendo-librerias-php/generate_pdf/html.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Extibax World</title>
+
+    <style>
+        h1 {
+            text-align: center;
+            background-color: black;
+            color: white;
+        }
+
+        p {
+            text-align: center;
+            margin: 15px auto;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Extibax World</h1>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo voluptate blanditiis exercitationem magnam voluptas fugit architecto ex assumenda natus fuga ipsa fugiat, velit culpa adipisci explicabo. Quidem expedita neque distinctio!
+    </p>
+</body>
+
+</html>
--- master-php/aprendiendo-librerias-php/generate_pdf/index.php
+++ master-php/aprendiendo-librerias-php/generate_pdf/index.php
@@ -1,13 +1,20 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require "../vendor/autoload.php";
 
 use Spipu\Html2Pdf\Html2Pdf;
 
 $html2pdf = new Html2Pdf();
 
-$html = "<h1>Hello World</h1>";
-$html .= "<p>Master en PHP</p>";
+//Recoger la vista a imprimir
+ob_start();
+require_once "html.php";
+$html = ob_get_clean();
+//End
 
 $html2pdf->writeHTML($html);
-$html2pdf->output("generete_pdf_1");
+$html2pdf->output("generete_pdf_1.pdf");
